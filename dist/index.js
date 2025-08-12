6f10d7f18d3ef2a06ab9ea8bda77a72570adc623
syma sultana <symasultana2002@gmail.com>
2025-08-13 00:33:44 +0600

    added files

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("hello world");
+let age = 25;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// console.log("hello world");
+// let age: number = 25;
+// if(age > 18) {
+//     console.log("You are an adult.");
+// }
+
+
+//another example
+let sales: 12345;
+
+let course: 'typeScript';
+// let isPublished: boolean = true;
+let isPublished: true;
+let level: any
+
+//literal means it takes only one value
